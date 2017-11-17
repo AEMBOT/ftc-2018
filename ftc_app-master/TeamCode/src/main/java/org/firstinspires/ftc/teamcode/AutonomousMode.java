fc845e14ebaf237838550e98eb515ec2765f856a
Will <jwilt9000@gmail.com>
2017-11-17 15:45:57 -0800

    AutoMode Updated

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -54,35 +54,15 @@ public class AutonomousMode extends LinearOpMode {
 
     private ColorSensor ColorSensor;
 
-    private VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-    private VuforiaTrackable relicTemplate = relicTrackables.get(0);
-    private RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+    private VuforiaTrackables relicTrackables;
+    private VuforiaTrackable relicTemplate;
+
+
+    public boolean VumarkFound = false;
 
     //All OpMode Code Is Stored Here And Ran From Here
     @Override
     public void runOpMode() throws InterruptedException {
-
-        //region Vuforia Variables
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        parameters.vuforiaLicenseKey = "AXYyTwz/////AAAAGSZopKRDQkUWstJxKlGUqq0dBRoXhOdaJSvoDa+Wq00FKnL" +
-                "AUYSqP4OENgv1Q9BVMXFj9LG6L69Wc3fbJuVL7ZetjnVLIzjd9Cn9hvh5rp6HiSJ1rFrlfx" +
-                "0sgtkHda7a/B7HivbiVjfXq+Dta1L3IgQ+GSEmvdkXioXG6kA5ZDpQ8yG2o4cyzzvWTBzBQbr" +
-                "Humt1ek8qcGYAiv+552WCDMdTvrMC+NQf5R+CQdzub9pHC1rHEu6fCpQT9oq+zM6Vk3TMwxQ8KWhII0AXh5815A0yCvSyMqQFX++empRQ" +
-                "j9o/hT6rSfz6hsHCeSg/RxptC2TpsfSos14e6rswW/Z/dh45fI3YTNfxxrkfzPjr2NAY";
-
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-
-
-
-        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
-
-        telemetry.addData(">", "Press Play to start");
-        telemetry.update();
-
-        //endregion
 
         //Assign Motor Variables To The Motor Names
         motorLF = hardwareMap.dcMotor.get("motorLF");
@@ -92,7 +72,20 @@ public class AutonomousMode extends LinearOpMode {
         ColorServo = hardwareMap.servo.get("CS");
         ColorSensor = hardwareMap.colorSensor.get("color");
 
-
+        //region Vuforia Variables
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AXYyTwz/////AAAAGSZopKRDQkUWstJxKlGUqq0dBRoXhOdaJSvoDa+Wq00FKnLAUYSqP4OENg" +
+                "v1Q9BVMXFj9LG6L69Wc3fbJuVL7ZetjnVLIzjd9Cn9hvh5rp6HiSJ1rFrlfx0sgtkHda7a/B7HivbiVjfXq+Dta1L3IgQ+" +
+                "GSEmvdkXioXG6kA5ZDpQ8yG2o4cyzzvWTBzBQbrHumt1ek8qcGYAiv+552WCDMdTvrMC+NQf5R+" +
+                "CQdzub9pHC1rHEu6fCpQT9oq+zM6Vk3TMwxQ8KWhII0AXh5815A0yCvSyMqQFX++empRQj9o/hT6rSf" +
+                "z6hsHCeSg/RxptC2TpsfSos14e6rswW/Z/dh45fI3YTNfxxrkfzPjr2NAY";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        //endregion
         motorLF.setDirection(DcMotor.Direction.REVERSE);
         motorLB.setDirection(DcMotor.Direction.REVERSE);
 
@@ -102,20 +95,21 @@ public class AutonomousMode extends LinearOpMode {
         //Actiavte The Relic Trackable
         relicTrackables.activate();
 
-        setUpVuforia(relicTemplate);
+        while (opModeIsActive()) {
+            setUpVuforia(relicTemplate);
+        }
 
     }
 
     //region Vuforia
     public void setUpVuforia(VuforiaTrackable relicTemplate) {
-
         /**
          * See if any of the instances of {@link relicTemplate} are currently visible.
          * {@link RelicRecoveryVuMark} is an enum which can have the following values:
          * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
          * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
          */
-
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
         if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
 
                 /* Found an instance of the template. In the actual game, you will probably
@@ -126,7 +120,7 @@ public class AutonomousMode extends LinearOpMode {
                 /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
                  * it is perhaps unlikely that you will actually need to act on this pose information, but
                  * we illustrate it nevertheless, for completeness. */
-            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
             telemetry.addData("Pose", format(pose));
 
                 /* We further illustrate how to decompose the pose into useful rotational and
@@ -145,18 +139,12 @@ public class AutonomousMode extends LinearOpMode {
                 double rY = rot.secondAngle;
                 double rZ = rot.thirdAngle;
             }
-        } else {
+        }
+        else {
             telemetry.addData("VuMark", "not visible");
         }
-
         telemetry.update();
-    }
-    String format(OpenGLMatrix transformationMatrix) {
-        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
-    }
 
-    public void ReadVuforia()
-    {
         if(vuMark.toString() == "CENTER")
         {
             telemetry.addData("It's The Center", "");
@@ -172,10 +160,11 @@ public class AutonomousMode extends LinearOpMode {
             telemetry.addData("It's The Right", "");
         }
     }
-
+    String format(OpenGLMatrix transformationMatrix) {
+        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+    }
 
     //endregion
-
 
 }
 
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousModeBasic.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousModeBasic.java
@@ -22,7 +22,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 //endregion
 
 //Define Program Name
-@Autonomous(name = "AutonomousMode")
+@Autonomous(name = "AutonomousModeBasic")
 public class AutonomousModeBasic extends LinearOpMode {
 
     //region Vuforia Stuffs
@@ -59,35 +59,10 @@ public class AutonomousModeBasic extends LinearOpMode {
     private RelicRecoveryVuMark vuMark;        //= RelicRecoveryVuMark.from(relicTemplate);
     OpenGLMatrix pose;
 
-    public boolean VumarkFound = false;
 
     //All OpMode Code Is Stored Here And Ran From Here
     @Override
     public void runOpMode() throws InterruptedException {
-
-        //region Vuforia Variables
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        parameters.vuforiaLicenseKey = "AXYyTwz/////AAAAGSZopKRDQkUWstJxKlGUqq0dBRoXhOdaJSvoDa+Wq00FKnL" +
-                "AUYSqP4OENgv1Q9BVMXFj9LG6L69Wc3fbJuVL7ZetjnVLIzjd9Cn9hvh5rp6HiSJ1rFrlfx" +
-                "0sgtkHda7a/B7HivbiVjfXq+Dta1L3IgQ+GSEmvdkXioXG6kA5ZDpQ8yG2o4cyzzvWTBzBQbr" +
-                "Humt1ek8qcGYAiv+552WCDMdTvrMC+NQf5R+CQdzub9pHC1rHEu6fCpQT9oq+zM6Vk3TMwxQ8KWhII0AXh5815A0yCvSyMqQFX++empRQ" +
-                "j9o/hT6rSfz6hsHCeSg/RxptC2TpsfSos14e6rswW/Z/dh45fI3YTNfxxrkfzPjr2NAY";
-
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        relicTemplate = relicTrackables.get(0);
-        vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
-
-        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
-
-        telemetry.addData(">", "Press Play to start");
-        telemetry.update();
-
-        //endregion
 
         //Assign Motor Variables To The Motor Names
         motorLF = hardwareMap.dcMotor.get("motorLF");
@@ -96,7 +71,6 @@ public class AutonomousModeBasic extends LinearOpMode {
         motorRB = hardwareMap.dcMotor.get("motorRB");
         ColorServo = hardwareMap.servo.get("CS");
         ColorSensor = hardwareMap.colorSensor.get("color");
-
 
         motorLF.setDirection(DcMotor.Direction.REVERSE);
         motorLB.setDirection(DcMotor.Direction.REVERSE);
@@ -107,84 +81,15 @@ public class AutonomousModeBasic extends LinearOpMode {
         //Actiavte The Relic Trackable
         relicTrackables.activate();
 
-        while (opModeIsActive()) {
-            setUpVuforia(relicTemplate);
-        }
-
-
+        motorLF.setPower(1);
+        motorRF.setPower(1);
+        motorLB.setPower(1);
+        motorRB.setPower(1);
+        Thread.sleep(500);
 
     }
 
-    //region Vuforia
-    public void setUpVuforia(VuforiaTrackable relicTemplate) {
-        telemetry.addData("Im Entering Vuforia", "");
-        /**
-         * See if any of the instances of {@link relicTemplate} are currently visible.
-         * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-         * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-         * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-         */
 
-        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-        telemetry.addData("VuMark Has Been Found", "");
-                /* Found an instance of the template. In the actual game, you will probably
-                 * loop until this condition occurs, then move on to act accordingly depending
-                 * on which VuMark was visible. */
-            telemetry.addData("VuMark", "%s visible", vuMark);
-
-                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
-                 * it is perhaps unlikely that you will actually need to act on this pose information, but
-                 * we illustrate it nevertheless, for completeness. */
-
-            telemetry.addData("Pose", format(pose));
-
-                /* We further illustrate how to decompose the pose into useful rotational and
-                 * translational components */
-            if (pose != null) {
-                VectorF trans = pose.getTranslation();
-                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-
-                // Extract the X, Y, and Z components of the offset of the target relative to the robot
-                double tX = trans.get(0);
-                double tY = trans.get(1);
-                double tZ = trans.get(2);
-
-                // Extract the rotational components of the target relative to the robot
-                double rX = rot.firstAngle;
-                double rY = rot.secondAngle;
-                double rZ = rot.thirdAngle;
-            }
-        } else {
-            telemetry.addData("VuMark", "not visible");
-        }
-
-        telemetry.update();
-        format(pose);
-    }
-    String format(OpenGLMatrix transformationMatrix) {
-        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
-    }
-
-    public void ReadVuforia()
-    {
-        while(true)
-        {
-            if (vuMark.toString() == "CENTER") {
-                telemetry.addData("It's The Center", "");
-            }
-
-            if (vuMark.toString() == "LEFT") {
-                telemetry.addData("It's The Left", "");
-            }
-
-            if (vuMark.toString() == "RIGHT") {
-                telemetry.addData("It's The Right", "");
-            }
-        }
-    }
-
-
-    //endregion
 
 
 }
